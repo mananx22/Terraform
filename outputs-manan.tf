2f01ab5d38111644fca202ad58b903381a4bfd3d
Ubuntu <ubuntu@ip-172-31-1-162.ap-south-1.compute.internal>
2021-08-23 10:11:57 +0000

    Module Outputs

--- outputs-manan.tf
+++ outputs-manan.tf
@@ -7,3 +7,13 @@
 #   value       = [for i in docker_container.nodered_container[*] : join(" | ", [i.name, i.ip_address, i.ports[0].external])]
 #   description = "The Name of container is "
 # }
+
+output "ContainerStyl1" {
+  value       = flatten(module.container[*].Container-Style1)
+  description = "The Name of container is "
+}
+
+output "ContainerStyl2" {
+  value       = flatten(module.container[*].Container-Style2)
+  description = "The Name of container is "
+}
